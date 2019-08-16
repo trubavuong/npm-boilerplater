da0544f1b6a43267ae4f4a7dd91ce8f3ac397da2
StevenTea <5902761+StevenTea@users.noreply.github.com>
2019-08-16 11:05:01 +0700

    Demo test async code.

--- src/person.js
+++ src/person.js
@@ -6,6 +6,14 @@ class Person {
   sayGreeting(targetName) {
     return `Hello ${targetName}! My name is ${this.name}.`;
   }
+
+  workInSeconds(seconds) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`I am ${this.name}. I have done in ${seconds} seconds.`);
+      }, seconds * 1e3);
+    });
+  }
 }
 
 export default Person;
--- src/person.spec.js
+++ src/person.spec.js
@@ -9,5 +9,13 @@ describe('person.js', () => {
         expect(greetingMessage).toEqual('Hello Mary! My name is Steven.');
       });
     });
+
+    describe('workInSeconds()', () => {
+      it('should work', async () => {
+        const person = new Person('Steven');
+        const finalMessage = await person.workInSeconds(5);
+        expect(finalMessage).toEqual('I am Steven. I have done in 5 seconds.');
+      }, 10e3);
+    });
   });
 });
