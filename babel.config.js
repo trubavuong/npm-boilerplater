9146b949d0b4574dc2f52a0b4280365c424cbe69
StevenTea <5902761+StevenTea@users.noreply.github.com>
2019-08-16 14:06:09 +0700

    Fix travis build with node v6.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = (api) => {
-  const isTest = api.env('test');
-  return {
-    ...(isTest ? { retainLines: true } : {}),
-    presets: [['@babel/env', { corejs: 3, useBuiltIns: 'usage' }]],
-  };
+  const presets = [['@babel/env', { corejs: 3, useBuiltIns: 'usage' }]];
+
+  const envConfigs = {};
+  if (api.env('test')) {
+    envConfigs.retainLines = true;
+  }
+
+  return Object.assign({}, { presets }, envConfigs);
 };
