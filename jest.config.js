8a5d86774f2bf93b8cac0dabee26d13c35b992ff
StevenTea <5902761+StevenTea@users.noreply.github.com>
2019-08-20 15:37:20 +0700

    Lint JS config files.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
@@ -24,7 +26,7 @@ module.exports = {
   // collectCoverageFrom: null,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -129,7 +131,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
